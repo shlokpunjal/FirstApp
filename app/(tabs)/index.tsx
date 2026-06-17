09646d07f3c24148e35fe3dd93e747fd39ecbb4a
Shlok Punjal <196046680+shlokpunjal@users.noreply.github.com>
2026-06-17 21:56:50 +0530

    modified:   app/(tabs)/index.tsx 	modified:   app/detail.tsx 	new file:   context/ThemeContext.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,8 +7,11 @@ export default function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home</Text>
-      <TouchableOpacity onPress={() => router.push('/detail')}>
-        <Text style={styles.btn}>Go to Detail</Text>
+      <Text style={styles.subtext}>This is the home</Text>
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={() => router.push('/detail')} >
+        <Text style={styles.btnText}>Go to Detail</Text>
       </TouchableOpacity>
     </View>
   )
@@ -16,6 +19,29 @@ export default function Home() {
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#0e0e0e' },
-  text: { color: '#fff', fontSize: 32 },
-  btn: {color: '#fff'}
+  text: {
+    color: '#fff',
+    fontSize: 32
+  },
+
+  btn: {
+    backgroundColor: '#dbdbdb',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 12,
+    marginVertical: 24,
+    borderWidth: 2,
+    borderColor: "#a1a1a1",
+  },
+
+  btnText: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+
+  subtext: {
+    color: '#b6b5b5', 
+    fontSize: 12,
+    marginTop: 8,
+  },
 })
--- app/detail.tsx
+++ app/detail.tsx
@@ -7,8 +7,10 @@ export default function Detail() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Detail Screen</Text>
-      <TouchableOpacity onPress={() => router.back()}>
-        <Text style={styles.back}>Go Back</Text>
+      <TouchableOpacity 
+      style={styles.btn}
+      onPress={() => router.back()}>
+        <Text style={styles.btnText}>Go Back</Text>
       </TouchableOpacity>
     </View>
   )
@@ -16,6 +18,23 @@ export default function Detail() {
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#1e1e1e' },
-  text: { color: '#fff', fontSize: 32, marginBottom: 24 },
-  back: { color: '#888', fontSize: 16 }
+  text: {
+    color: '#fff',
+    fontSize: 32, 
+    marginBottom: 16,
+  },
+
+  btn: {
+    backgroundColor: '#dbdbdb',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 12,
+    borderWidth: 2,
+    borderColor: "#a1a1a1",
+  },
+
+  btnText: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
 })
